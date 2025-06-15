71df58059a30928965c672780bb95291617014e9
Harshvardhan Handa <Harshvardhan.Handa.ug21@nsut.ac.in>
2025-06-15 08:02:03 +0530

    adding code for backend

---
+++ employee-management-backend/employee-mangement-backend/src/test/java/com/example/employee_mangement_backend/EmployeeMangementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_mangement_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMangementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
